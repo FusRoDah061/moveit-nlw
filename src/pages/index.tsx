854277e7212b5ef2a9a080384e5487a388bceffb
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-02-24 18:46:38 -0300

    chore: Migrate home page to css modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Head from 'next/head';
 import { ExperienceBar } from "../components/ExperienceBar";
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-            rel="stylesheet" />
+        <title>Início | move.it</title>
       </Head>
+
       <ExperienceBar />
     </div>
   )
