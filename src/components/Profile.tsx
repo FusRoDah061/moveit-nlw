011000150127464c7f56ce97d8dcd3feef89d925
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-02-25 18:42:55 -0300

    fix: Update user level on level up

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/FusRoDah061.png" alt="Allex Rodrigues"/>
@@ -9,7 +13,7 @@ export function Profile() {
         <strong>Allex Rodrigues</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
